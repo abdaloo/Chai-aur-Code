541f0b6e8843d856c73e362f145f43426f312506
Abdal Hussain <abdalhussain5790@gmail.com>
2024-11-24 15:15:05 +0500

    Null and Undefined Comparison with zero

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,20 @@
-let gameCounter = 20;
+// console.log("2" > 0);
+// console.log("02" > 0);
 
-console.log(--gameCounter);
 
-// console.log(gameCounter);
+// console.log("2" === 2);
+// console.log("2" == 2);
+
+console.log(null < 0);
+console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+
+
+
 
 
