4e12634140712ba2e265806af170ff307fbdd1c0
Abdal Hussain <abdalhussain5790@gmail.com>
2024-11-25 23:33:10 +0500

    Strings and its methods

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,17 +1,95 @@
-// console.log("2" > 0);
-// console.log("02" > 0);
+//Task: Write a function that checks if the string "JavaScript" exists in a given string.
+function containsJavaScript(str) {
+    
+    return str.includes("JavaScript");
+  }
+  
+  console.log(containsJavaScript("I love JavaScript!")); // true
+  console.log(containsJavaScript("I love JavaScript!")); // false
+  
+  //split, reverse, join
+  function reverseString(str) {
+    // Your code here
+    const charArray = str.split('');
+    const reverse = charArray.reverse();
+    const reversedString = reverse.join('');
+    return reversedString;
+
+  }
+  
+  console.log(reverseString("JavaScript")); // "tpircSavaJ"
+  
 
 
-// console.log("2" === 2);
-// console.log("2" == 2);
 
-console.log(null < 0);
-console.log(null == 0);
-// console.log(null >= 0);
 
-// console.log(undefined == 0);
-// console.log(undefined < 0);
-// console.log(undefined > 0);
+/*
+ƒ anchor()
+ƒ at()
+ƒ big()
+ƒ blink()
+ƒ bold()
+ƒ charAt()
+ƒ charCodeAt()
+ƒ codePointAt()
+ƒ concat()
+ƒ String()
+ƒ endsWith()
+ƒ fixed()
+ƒ fontcolor()
+ƒ fontsize()
+ƒ includes()
+ƒ indexOf()
+ƒ isWellFormed()
+ƒ italics()
+ƒ lastIndexOf()
+ƒ link()
+ƒ localeCompare()
+ƒ match()
+ƒ matchAll()
+ƒ normalize()
+ƒ padEnd()
+ƒ padStart()
+ƒ repeat()
+ƒ replace()
+ƒ replaceAll()
+ƒ search()
+ƒ slice()
+ƒ small()
+ƒ split()
+ƒ startsWith()
+ƒ strike()
+ƒ sub()
+ƒ substr()
+ƒ substring()
+ƒ sup()
+ƒ toLocaleLowerCase()
+ƒ toLocaleUpperCase()
+ƒ toLowerCase()
+ƒ toString()
+ƒ toUpperCase()
+ƒ toWellFormed()
+ƒ trim()
+ƒ trimEnd()
+ƒ trimStart()
+ƒ trimEnd()
+ƒ trimStart()
+ƒ valueOf()
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- 01_basics/first.js
+++ 01_basics/first.js
@@ -1,1 +1,3 @@
 console.log('This is first line of code of JavaScript');
+
+console.log("HI");
