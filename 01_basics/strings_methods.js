7bdc7aa7ebfc40dfa4790832f9c83723b03d3e41
Abdal Hussain <abdalhussain5790@gmail.com>
2024-11-26 20:47:04 +0500

    Number and Math

---
+++ 01_basics/strings_methods.js
@@ -0,0 +1,128 @@
+//"Hello, World!";
+/*Q:1
+const word = "Hello, World!";
+let wordO = word.split(",")[1];
+console.log(wordO);//Extraction of Hello
+console.log(word.toUpperCase());//UPpercase
+console.log(word.replace('World!',"JavaScript"));//Changes of Word
+*/
+
+/* Q:2
+//Check for Subtrings
+function containsJavaScript(str) {
+  let word = str.includes("JavaScript");
+  return word;
+}
+
+console.log(containsJavaScript("I love JavaScript!")); // true
+console.log(containsJavaScript("I love Python!")); // false
+console.log(containsJavaScript("I am learning JavaScript because of my passion."));
+*/
+/*
+//Q:3
+//Reverse a string
+function reverseString(str) {
+  const charArray = str.split('');
+  const reverse = charArray.reverse();
+  const reversedWord = reverse.join('');
+  return reversedWord;
+}
+
+console.log(reverseString("JavaScript")); // "tpircSavaJ"
+*/
+
+//Q:4
+function countVowels(str) {
+  // Your code here
+}
+
+console.log(countVowels("JavaScript is awesome")); // 8
+console.log();
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+/*
+ƒ anchor()
+ƒ at()
+ƒ big()
+ƒ blink()
+ƒ bold()
+ƒ charAt()
+ƒ charCodeAt()
+ƒ codePointAt()
+ƒ concat()
+ƒ String()
+ƒ endsWith()
+ƒ fixed()
+ƒ fontcolor()
+ƒ fontsize()
+ƒ includes()
+ƒ indexOf()
+ƒ isWellFormed()
+ƒ italics()
+ƒ lastIndexOf()
+ƒ link()
+ƒ localeCompare()
+ƒ match()
+ƒ matchAll()
+ƒ normalize()
+ƒ padEnd()
+ƒ padStart()
+ƒ repeat()
+ƒ replace()
+ƒ replaceAll()
+ƒ search()
+ƒ slice()
+ƒ small()
+ƒ split()
+ƒ startsWith()
+ƒ strike()
+ƒ sub()
+ƒ substr()
+ƒ substring()
+ƒ sup()
+ƒ toLocaleLowerCase()
+ƒ toLocaleUpperCase()
+ƒ toLowerCase()
+ƒ toString()
+ƒ toUpperCase()
+ƒ toWellFormed()
+ƒ trim()
+ƒ trimEnd()
+ƒ trimStart()
+ƒ trimEnd()
+ƒ trimStart()
+ƒ valueOf()
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
