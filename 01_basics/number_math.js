2c93bf66527014d3beb0bdf42595e8bcffc71dbe
Abdal Hussain <abdalhussain5790@gmail.com>
2024-11-26 21:22:24 +0500

    Number and Math

---
+++ 01_basics/number_math.js
@@ -0,0 +1,17 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+const other = 123.343233;
+console.log(other.toPrecision(5));
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString());
+
+
