e7604771cf7dfe1f641f922dc401c3706be4157f
Abdal Hussain <abdalhussain5790@gmail.com>
2024-11-28 19:22:49 +0500

    Date and Time

---
+++ 01_basics/date_time.js
@@ -0,0 +1,57 @@
+let date1 = new Date();
+
+// console.log(date1.toString());
+// console.log(date1.toLocaleString());
+// console.log(date1.toLocaleDateString());
+// console.log(date1.toLocaleTimeString());
+// console.log(date1.toDateString());
+// console.log(date1.toTimeString());
+
+// console.log(typeof date1);
+
+let myDate1 = new Date(2024,10,27);
+let myDate2 = new Date(2024,10,27,9,3,0);
+let myDate3 = new Date("11-27-2024,9:3");
+
+// console.log(myDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myDate1.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+// console.log(newDate.getMilliseconds());
+// console.log(newDate.getMinutes());
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getSeconds());
+// console.log(newDate.getTime());
+// console.log(newDate.getTimezoneOffset());
+// console.log(newDate.getUTCDate());
+
+// newDate.toLocaleString('default',{
+//     weekday: "long",
+  
+// })
+
+//Q:1 => Write a function to dipslay the current date and time in the format:- YYYY-MM-DD HH:mm:ss
+// function showDateAndTime(date){
+// let my_Date = new Date();
+// my_Date = date;
+// return my_Date.toLocaleString();
+// }
+// console.log(showDateAndTime("11-27-2024 , 9:34:23"));
+//                or
+// let my_Date = new Date();
+// console.log(my_Date.toLocaleString());
+
+
+      
+
+
+
+
+
