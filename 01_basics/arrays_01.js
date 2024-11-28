6817317618fe449959d02f08c6f634834226957e
Abdal Hussain <abdalhussain5790@gmail.com>
2024-11-28 21:08:34 +0500

    Arrays Part One

---
+++ 01_basics/arrays_01.js
@@ -0,0 +1,38 @@
+//Arrays declaring method => Part 1
+const myArr = [9,8,7,6,5];
+const myString = ["Abdal","Hussain"];
+const myArray = new Array(10,2,3,4)
+
+// myArray.push(100);
+// myArray.pop();
+// myArray.unshift(8);
+// myArray.shift();
+
+// console.log(myArray.indexOf(4));
+// console.log(myArray.includes(4));
+
+const newArray = myArray.join();
+
+// console.log(myArray);
+// console.log(typeof myArray);
+
+// console.log(newArray);
+// console.log(typeof newArray);
+
+// //slice and splice
+// console.log("A ", myArray);
+const myN1 = myArray.slice(1,3);
+// console.log(myN1);
+
+// console.log("B", myArray);
+const myN2 = myArray.splice(1,3);
+// console.log(myN2);
+// console.log(myArray);
+
+
+
+
+
+
+
+
