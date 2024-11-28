3b23898f948eebc5c7f6997c943483494c797091
Abdal Hussain <abdalhussain5790@gmail.com>
2024-11-28 21:44:01 +0500

    Arrays Topic End

--- 01_basics/arrays_01.js
+++ 01_basics/arrays_01.js
@@ -29,6 +29,36 @@ const myN2 = myArray.splice(1,3);
 // console.log(myN2);
 // console.log(myArray);
 
+// +++++++++++++>>>>>>> :Part Two: <<<<<<<<++++++++++
+const marvel_heroes = ["Ironman","Spiderman","Hulk"];
+const dc_heroes = ["Superman","Flash","Batman"];
+
+// marvel_heroes.push(dc_heroes);//This will Add Dc Value as 3rd Element.
+// console.log(marvel_heroes);
+
+// const All_heroes = marvel_heroes.concat(dc_heroes);//concat() will add two arrays and combine them into one array
+// console.log(All_heroes[2]);
+
+//For this purpose also used spread operator
+// const newHeroes = [...marvel_heroes,...dc_heroes];
+// console.log(newHeroes);
+let real_new_array = [1,2,3,[2,3,5],[7,9,[45,99]]];
+real_new_array = real_new_array.flat(Infinity);//flat() will merge it into one array,we can also add value like 1,2,3 etc and infinity also.
+// console.log(real_new_array);
+
+// console.log(Array.isArray("Abdal"));
+// console.log(Array.from("Abdal"));//give us date in the form of ArrayS
+// console.log(Array.from({name: "hitesh"}));//
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1,score2,score3));//Array.Of() method combile value in the form of array
+
+
+
+
 
 
 
