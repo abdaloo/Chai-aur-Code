fb78f972827d5b62b9087f78bcfb6b501c108765
Abdal Hussain <abdalhussain5790@gmail.com>
2024-11-30 22:16:05 +0500

    Objects

---
+++ objects.js
@@ -0,0 +1,24 @@
+let cars = {
+    name: "BMW",
+    model: "2004",
+    color: "Red",
+}
+
+let person = {};
+
+person.name = "Abdal";
+person.fatherName = "Muhammad Hussain";
+person.subject = "Computer Science";
+
+let person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+
+  console.log(person.fullName);
+  
+
