758532f43bba56c501520286450b8144b4486406
Abdal Hussain <abdalhussain5790@gmail.com>
2024-12-04 21:30:49 +0500

    Objects de Structuring and API intro

---
+++ 01_basics/obj_d_structure_JSON_API.js
@@ -0,0 +1,17 @@
+const course = {
+    courseName: "Chai aur Code",
+    coursePrice: "999",
+    courseInstructure: "Hitesh Choudray",
+}
+
+// console.log(course.courseInstructure);
+
+const {courseName: you} = course;//object de-Structuring Methods
+console.log(you);
+
+
+
+
+
+
+
