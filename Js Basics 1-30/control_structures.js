e24ad2d30df8286587d849df5152ab430dc83ac2
Abdal Hussain <abdalhussain5790@gmail.com>
2024-12-13 20:05:09 +0500

    Statements

--- Js Basics 1-30/control_structures.js
+++ Js Basics 1-30/control_structures.js
@@ -1,4 +1,22 @@
-if(true){
-    console.log("This is Abdal");
+
+
+//+++++++++++++++
+let age = 17;
+let hasLicense = true;
+
+if(age >= 18){
+    console.log("You are old enough to drive");
+    
+    if(hasLicense){
+        console.log("You have your licence");
+        
+    }
+    else{
+        console.log("You do not have your license yet!");
+        
+    }
+}
+else{
+    console.log("you must be 18+ to have a licence");
     
 }
