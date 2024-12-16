56609b7a0456fd5bff1d0838bf8682146d2babef
Abdal Hussain <abdalhussain5790@gmail.com>
2024-12-16 08:19:39 +0500

    Iteration

---
+++ Js Basics 1-30/iteration.js
@@ -0,0 +1,27 @@
+// for(let i = 0; i<=10; i++){
+//     const element = i;
+//     if(element == 3){
+//         continue
+//     }
+//     console.log(element)
+// }
+
+// for(let i = 1; i<=10; i++){
+//     console.log(`This is ${i} Table:`);
+    
+//     for(let j = 1; j<=10; j++){
+//        // console.log(`Inner loop value ${j} and inner loop ${i}`);
+//         console.log(i + '*' + j + ' = ' + i*j);
+//     }
+// }
+
+let myArray = ["flash","batman","superman"]
+
+console.log(myArray.length);
+
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
+}
