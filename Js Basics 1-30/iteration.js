d028cd732fdc006bb89643202c836fbb20edb17f
Abdal Hussain <abdalhussain5790@gmail.com>
2024-12-16 08:55:01 +0500

    While and do while

--- Js Basics 1-30/iteration.js
+++ Js Basics 1-30/iteration.js
@@ -17,11 +17,12 @@
 
 let myArray = ["flash","batman","superman"]
 
-console.log(myArray.length);
+//console.log(myArray.length);
 
 
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
-    console.log(element);
+    //console.log(element);
     
 }
+
